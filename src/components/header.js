48eed2f1eb762dc59d9d8e9e79fd22deea9a62aa
Vido <vidoseaver@gmail.com>
2018-07-22 14:48:46 -0600

    Update filename

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
 
     return (
         <View style={viewStyle} >
-            <Text style={textStyle}>{props.headerText}</Text>
+            <Text style={textStyle}> {props.headerText} </Text>
         </View>
     )   
 }

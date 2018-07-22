593a12660c5d8c3e0b9beb2bcb1e7b81a4126115
Vido <vidoseaver@gmail.com>
2018-07-22 13:49:58 -0600

    Add header with styling

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/** @format */
+// Import libraries to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a component
+const App = () => (
+    <Header headerText={'Albums!'}/>
+)
 
-AppRegistry.registerComponent(appName, () => App);
+// Render it
+
+AppRegistry.registerComponent('albums', () => App)

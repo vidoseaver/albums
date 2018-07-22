ede71ef97beaa073897424bb7569640bf86ef4ce
Vido <vidoseaver@gmail.com>
2018-07-22 14:48:28 -0600

    Change import to pull from Header and add AlbumList component

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Import libraries to help create a component
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header'
+import React from 'react'
+import { View, AppRegistry } from 'react-native'
+import Header from './src/components/Header'
+import AlbumList from "./src/components/AlbumList"
 
 // Create a component
-const App = () => (
-    <Header headerText={'Albums!'}/>
-)
+const App = () => {
+    return (
+        <View>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+        </View>
+    )
+};
+
+
 
 // Render it
 
